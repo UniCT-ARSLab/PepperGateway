9d736e1da7aff35fb2c19333bed95a54f3c4fb64
Giuseppe Pitruzzella <giuseppepitru@icloud.com>
2022-11-29 19:29:27 +0100

    :bug: Fix flask_sqlalchemy

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
-build:
+build-main:
 	docker build -t pepper-gateway .
 
-run:
+run-main:
 	docker run --rm -it -p 6566:6566 -v /Users/giuseppepitruzzella/PepperGateway:/app/ --name gw pepper-gateway
+
+build-env:
+	docker build -t pepper-env -f pepper-env .  
+
+run-env:
+	docker run --rm -it -p 6566:6566 -v /Users/giuseppepitruzzella/PepperGateway/:/app/ --name gw pepper-env
--- gateway/app.py
+++ gateway/app.py
@@ -1,17 +1,15 @@
-# coding=utf-8
-
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from time import sleep
-# from PepperGateway import robot
-# import config
-# import time
 
-# FIX: Affinché possa inizializzare il robot, devo importare il modulo robot.py
-# Per importare robot.py, devo considerare di copiare l'intera directory PepperGateway all'interno del container 
-# e.g. -v /Users/giuseppepitruzzella/PepperGateway:/PepperGateway OR COPY ../ .
 
-# pepper = Pepper(config.IP_ADDRESS, config.PORT)
+import sys
+sys.path.append('..')
+from .. import *
+
+import config
+import time
+
+pepper = Pepper(config.IP_ADDRESS, config.PORT)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -25,7 +23,6 @@ class target(db.Model):
     y = db.Column(db.Integer)
     theta = db.Column(db.Integer)
 
-
 @app.route('/')
 def index():
     target_list = target.query.all() # SELECT * FROM target;
@@ -36,21 +33,17 @@ def index():
 @app.route('/add', methods=['POST'])
 def add():
     # Add new target to the database
-
     sleep(3) # Delay useful to display form animation
     target_name = request.form.get('target_name')
 
-    # values = pepper.setup_point(target_name)
+    # X, Y, Theta will be returned from the setup_point() in robot.py
+    # target_x = request.form.get('x')
+    # target_y = request.form.get('y')
+    # target_theta = request.form.get('theta')
 
-    # target_x = values[0]
-    # target_y = values[1]
-    # target_theta = values[2]
-
-    # new_target = target(text = target_name, x = target_x, y = target_y, theta = target_theta)
-    new_target = target(text = target_name, x = 0, y = 0, theta = 0)
+    new_target = target(text=target_name, x=0, y=0, theta=0)
     db.session.add(new_target)
     db.session.commit()
-
     return redirect(url_for('index'))
 
 @app.route('/delete/<int:id>')
