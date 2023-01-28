e52fad6d987111adef0bc72a0fee7c7761f339e2
Jax <45317135+GiuseppePitruzzella@users.noreply.github.com>
2023-01-28 19:28:43 +0100

    :sparkles: Add joypad mode

--- gateway/WebServer.py
+++ gateway/WebServer.py
@@ -18,7 +18,7 @@ class Target(db.Model):
     theta = db.Column(db.Integer)
 
 class WebServer:
-    def __init__(self, robot):
+    def __init__(self, robot = None):
         self.robot = robot    
         self.defineRoutes()
         self.startServer()
@@ -40,16 +40,17 @@ class WebServer:
             targetList = Target.query.all() # SELECT * FROM target;
             return render_template('index.html', targetList=targetList)
 
-        @app.route('/environment/')
-        def environment():
-            return render_template('environment.html')
+        @app.route('/pointsOfInterests')
+        def pointsOfInterests():
+            return render_template('pointsOfInterest.html')
+
+        @app.route('/joypadMode')
+        def joypadMode():
+            return render_template('joypadMode.html')
 
         @app.route('/add', methods=['POST'])
         def add():
-            sleep(3) # Delay useful to display form animation
             target_name = request.form.get('target_name')
-
-            # item = Target(text=target_name, x=0, y=0, theta=0)
 
             self.robot.robot_localization()
             item = Target(text=target_name, 
@@ -59,14 +60,14 @@ class WebServer:
             
             db.session.add(item)
             db.session.commit()
-            return redirect(url_for('index'))
+            return redirect(url_for('pointsOfInterest'))
 
         @app.route('/delete/<int:id>')
         def delete(id):
             item = Target.query.filter_by(id=id).first()
             db.session.delete(item)
             db.session.commit()
-            return redirect(url_for('index'))
+            return redirect(url_for('pointsOfInterest'))
 
         @app.route('/get/<int:id>')
         def get(id):
@@ -75,28 +76,25 @@ class WebServer:
             # self.robot.say("Eseguo lo spostamento verso " + item.text)
             # self.robot.robot_localization()
             # self.robot.navigate_to(item.x, item.y, item.theta)
-            return redirect(url_for('index'))
+            return redirect(url_for('pointsOfInterest'))
 
         @app.route('/say', methods=['POST'])
         def say():
             self.robot.say(request.form.get('text'))
             return redirect(url_for('index'))
-
-        @app.route('/awareness_on', methods=['POST'])
-        def awareness_on():
-            self.robot.set_awareness(True)
-            return redirect(url_for('index'))
         
-        @app.route('/awareness_off', methods=['POST'])
-        def awareness_off():
-            self.robot.set_awareness(False)
-            return redirect(url_for('index'))
-        
-        @app.route('/rotate')
+        @app.route('/rotate', methods=['POST'])
         def rotate():
-            self.robot.say("Avvio rotazione")
-            self.robot.turn_around(0.1)
-            return redirect(url_for('index'))
+            print("Test passed!")
+            # self.robot.say(request.form.get('data'))
+            # self.robot.turn_around(request.form.get('data'))
+            return redirect(url_for('environment'))
+
+        @app.route('/move', methods=['POST'])
+        def move():
+            print(request.form.get('data'))
+            return redirect(url_for('environment'))
+            # self.robot.move_forward(request.form.get('data'))
     
 
     
--- gateway/static/js/environment.js
+++ gateway/static/js/environment.js
@@ -1,53 +1,160 @@
-var rotate_item = nipplejs.create({
-    zone: document.getElementById('static-rotate'),
-    mode: 'static',
-    lockX: true,
-    position: 'absolute',
-    color: 'red'
-});
-var forward_item = nipplejs.create({
-    zone: document.getElementById('static-forward'),
-    mode: 'static',
-    lockY: true,
-    position: 'absolute',
-    color: 'blue'
-});
+var joysticks = {
+    forward_joystick: {
+        zone: document.getElementById('static-forward'),
+        mode: 'static',
+        position: 'absolute',
+        lockY: true,
+        color: 'blue'
+    },
+    rotate_joystick: {
+        zone: document.getElementById('static-rotate'),
+        mode: 'static',
+        position: 'absolute',
+        lockX: true,
+        color: 'red'
+    }
+};
+var joystick;
 
+// Get debug elements and map them
+var elDebug = document.getElementById('debug');
+console.log(elDebug);
+var elDump = elDebug.querySelector('.dump');
+var els = {
+  position: {
+    x: elDebug.querySelector('.position .x .data'),
+    y: elDebug.querySelector('.position .y .data')
+  },
+  force: elDebug.querySelector('.force .data'),
+  pressure: elDebug.querySelector('.pressure .data'),
+  distance: elDebug.querySelector('.distance .data')
+};
 
-// rotate_item.on('start', function (evt, nipple) {
-//     nipple.on('move end dir plain', function (evt) {
-//         console.log("ROTATE")
-//         console.log("X POSITION:" + nipple.position.x + " Y POSITION:" + nipple.position.y)
-//         console.log("FORCE:" + nipple.force + "    DISTANCE:" + nipple.distance + "    PRESSURE" + nipple.pressure)
-//         // console.log("RADIAN:" + nipple.angle.degree)
-//     });
-// });
+createNipple('forward_joystick');
+createNipple('rotate_joystick');
 
-// forward_item.on('start', function (evt, nipple) {
-//     nipple.on('move end dir plain', function (evt) {
-//         console.log("FORWARD")
-//         console.log("X POSITION:" + nipple.position.x + " Y POSITION:" + nipple.position.y)
-//         console.log("FORCE:" + nipple.force + "    DISTANCE:" + nipple.distance + "    PRESSURE" + nipple.pressure)
-//         // console.log("RADIAN:" + nipple.angle.degree)
-//     });
-// });
+function bindNipple() {
+    joystick.on('move', function(evt, data) {
+        setTimeout(function() {
+            debug(data);
+        }, 2000);
+    });
 
+    // joystick.on('start end', function(evt, data) {
+    //     // dump(evt.type);
+    //     debug(data);
+    // }).on('move', function(evt, data) {
+    //     debug(data);
+    // }).on('dir:up plain:up dir:left plain:left dir:down ' +
+    //       'plain:down dir:right plain:right',
+    //       function(evt, data) {
+    //     // dump(evt.type);
+    // }).on('pressure', function(evt, data) {
+    //     debug({
+    //         pressure: data
+    //     });
+    // });
+}
 
-rotate_item.on('move', async function (evt, nipple) {
-    console.log("Avvio rotazione!")
-    let index = 0
-    const response = await fetch("/rotate", { method: "GET" })
-    // const data = {
-    //     "distance": nipple.distance,
-    //     "radian": nipple.angle.radian,
-    //     "position": nipple.position,
+function createNipple(evt) {
+    var type = typeof evt === 'string' ?
+        evt : evt.target.getAttribute('data-type');
+    // if (joystick) {
+    //   joystick.destroy();
     // }
-    // try {
-    //     const response = await fetch("/rotate", {
-    //         method: "POST",
-    //         data: JSON.parse(data)
-    //     })
-    // } catch (error) {
-    //     console.log("Something went wrong")
-    // }
-});
+    joystick = nipplejs.create(joysticks[type]);
+    bindNipple();
+}
+
+// Print data into elements
+function debug(obj) {
+    function parseObj(sub, el) {
+    	for (var i in sub) {
+        	if (typeof sub[i] === 'object' && el) {
+          		parseObj(sub[i], el[i]);
+        	} else if (el && el[i]) {
+        		el[i].innerHTML = scale(sub[i]);
+                setTimeout(function() {
+                    makeRequests(scale(sub["force"]));
+                }, 2000);
+        }
+      }
+    }
+    setTimeout(function() {
+      parseObj(obj, els);
+    }, 2000);
+  }
+  
+var nbEvents = 0;
+  
+  // Dump data
+function dump(evt) {
+    // setTimeout(function() {
+    //     if (elDump.children.length > 4) {
+    //         elDump.removeChild(elDump.firstChild);
+    //     }
+    // var newEvent = document.createElement('div');
+    // newEvent.innerHTML = '<span class="data">' +
+    //     evt + '</span>';
+    //     elDump.appendChild(newEvent);
+    //     nbEvents += 1;
+    // }, 0);
+}
+
+function scale (number, inMin = 0, inMax = 10, outMin = 0, outMax = 3) {
+	return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+async function fetchWithTimeout(resource, options = {}) {
+    const { timeout = 8000 } = options;
+    
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+    const response = await fetch(resource, {
+      ...options,
+      signal: controller.signal  
+    });
+    clearTimeout(id);
+    return response;
+}
+
+// setInterval(
+//     async function send(movement) {
+//     try {
+//         const response = await fetch("/move", {
+//             method: "POST",
+//             data: JSON.parse(movement)
+//         })
+//     } catch (error) {
+//         console.log("Something went wrong")
+//     }
+// }, 5000);
+
+// async function send(movement) {
+//     // axios.post('/rotate', {
+//     //     data: movement
+//     // })
+//     // .then(function (response) {
+//     //     console.log(response);
+//     // })
+//     // .catch(function (error) {
+//     //     console.log(error);
+//     // });
+//     console.log(movement);
+// }
+
+// async function sendData(movement) { 
+//     return new Promise(async(resolve, reject) => {
+//         console.log(movement);
+//         return resolve();
+//     })
+// }
+async function makeRequests (movement) {
+    axios.post('/rotate', { data: movement }, { timeout: 2000 })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
